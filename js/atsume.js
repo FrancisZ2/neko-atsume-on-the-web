d05b1f9387347178a769a1358e03a253a0eefae4
kh1550 <kristin.dramedij60@gmail.com>
2016-04-26 16:16:09 -0400

    half through toy locations

--- js/atsume.js
+++ js/atsume.js
@@ -1,10 +1,29 @@
 var bg;
+var width, height, cax, caw;
+var tloc = [];
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  width = window.innerWidth;
+  height = window.innerHeight;
+  cax = width/8;
+  caw = width * (3/4);
+  createCanvas(width,height);
   bg = loadImage('img/bg.png');
+
+  // SPOT ORDER: stone, bridge, red porch, center top porch, center bottom porch, tree, left porch, tatami
+  tloc.push([(caw/12) + cax, height * (3/7)]);
+  tloc.push([(caw * (2/5)) + cax, height * (3/14)]);
+  tloc.push([(caw * (7/28)) + cax, height * (3/5)]);
+  tloc.push([(caw * (3/5)) + cax, height * (3/7)]);
+  tloc.push([(caw * (3/7)) + cax, height * (5/7)]);
+  console.log(tloc);
 }
 
 function draw() {
-  image(bg,0,0,600,450);
+  background(51);
+  image(bg,cax,0,caw,height);
+  imageMode(CENTER);
+  rect((caw * (3/7)) + cax, height * (5/7),50,50);
 }
+
+// add eventlistener for window resize
