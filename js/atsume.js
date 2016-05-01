8459aaa8f4fda176bd4382f43dd01baa4a4b20aa
kh1550 <kristin.dramedij60@gmail.com>
2016-05-01 15:32:41 -0400

    all toys/cats in place, testing size compatibility

--- js/atsume.js
+++ js/atsume.js
@@ -1,61 +1,85 @@
-var bg, cafe, temari, sfran, marsh, kath;
+var bg, goods = [], cats = [];
 var width, height, cax, caw;
+var wratio, hratio, myw = 1440, myh = 955, myratio = myw/myh;
 var tloc = [];
-var cloc = {
-  "cafe-right": [],
-  "cafe-left": [],
-  "cafe-top": [],
-};
+var cloc = [];
 
 function setup() {
   width = window.innerWidth;
   height = window.innerHeight;
-  cax = width/8;
-  caw = width * (3/4);
-  createCanvas(width,height);
   bg = loadImage('img/bg.png');
 
+  if (false) { // thinking this part through
+  if ((width/height) != myratio) {
+    // find which dimension must have a padding
+    var hway = height * (width/bg.width);
+    var wway = width * (height/bg.height);
+  } else {
+    wratio = width/myw;
+    hratio = height/myh;
+    cax = 0;
+    cay = 0;
+    caw = width;
+  }
+}
+  cax = width/8;
+  caw = width * (3/4);
+  console.log(height);
+  createCanvas(width,height);
+
   // GOODIES
-  cafe = loadImage('img/cafe.png');
-  temari = loadImage('img/temari.png');
+  goods = [
+    loadImage('img/bucket.png'), loadImage('img/pyramid.png'),
+    loadImage('img/cafe.png'), loadImage('img/temari.png'),
+    loadImage('img/sakura.png'), loadImage('img/royal.png'),
+    loadImage('img/kettle.png'), loadImage('img/rails.png')
+  ];
 
   // CATS
-  sfran = loadImage('img/sassyfran.png');
-  marsh = loadImage('img/marshmallow.png');
-  kath = loadImage('img/kathmandu.png');
+  cats = [
+    loadImage('img/shadow.png'), loadImage('img/pepper.png'),
+    loadImage('img/marshmallow.png'), loadImage('img/sassyfran.png'),
+    loadImage('img/breezy.png'), loadImage('img/kathmandu.png'),
+    loadImage('img/meowgi.png'), loadImage('img/xerxes.png'),
+    loadImage('img/ginger.png'), loadImage('img/conductor.png')
+  ]
 
   // SPOT ORDER: stone, bridge, red porch, center top porch, center bottom porch, tree, left porch, tatami
-  tloc.push([(caw/12) + cax, height * (3/7)]);
-  tloc.push([(caw * (2/5)) + cax, height * (3/14)]);
+  tloc.push([(caw * 0.10) + cax, height * 0.45]);
+  tloc.push([(caw * 0.42) + cax, height * 0.24]);
   tloc.push([(caw * (8/28)) + cax, height * (3/5)]);
-  tloc.push([(caw * (3/5)) + cax, height * (3/7)]);
-  tloc.push([(caw * (3/7)) + cax, height * (5/7)]);
-  tloc.push([(caw * (9/14)) + cax, height * (23/28)]);
-  tloc.push([(caw * (6/7)) + cax, height * (5/7)]);
-  tloc.push([(caw * (23/28)) + cax, height * (15/28)]);
+  tloc.push([(caw * 0.64) + cax, height * 0.48]);
+  tloc.push([(caw * 0.45) + cax, height * 0.75]);
+  tloc.push([(caw * 0.66) + cax, height * 0.80]);
+  tloc.push([(caw * 0.88) + cax, height * 0.75]);
+  tloc.push([(caw * 0.84) + cax, height * 0.58]);
 
-  // CAFE
-  cloc["cafe-left"].push([(caw * (12/56)) + cax, height * (9/14)]);
-  cloc["cafe-top"].push([(caw * (8/28)) + cax, height * (13/28)]);
+  // CAT ORDER: bucket, pyramid, cafe top, cafe left, cafe right, temari, sakura, royal bed, kettle, rails
+  cloc.push([(caw * 0.10) + cax, height * 0.45]);       // shadow
+  cloc.push([(caw * 0.4055) + cax, height * 0.285]);    // pepper
+  cloc.push([(caw * (8/28)) + cax, height * 0.455]);    // marshmallow
+  cloc.push([(caw * (12/56)) + cax, height * (9/14)]);  // sassy fran
+  cloc.push([(caw * 0.32) + cax, height * 0.607]);      // breezy
+  cloc.push([(caw * (3/5)) + cax, height * (3/7)]);     // kathmandu
+  cloc.push([(caw * 0.45) + cax, height * 0.70]);       // mr. meowgi
+  cloc.push([(caw * 0.66) + cax, height * 0.80]);       // xerxes
+  cloc.push([(caw * 0.885) + cax, height * 0.67]);      // ginger
+  cloc.push([(caw * 0.77) + cax, height * 0.48]);       // conductor whiskers
 }
 
 function draw() {
   background(51);
   image(bg,cax,0,caw,height);
-  imageMode(CENTER);
   for (var i = 0; i < tloc.length; i++) {
-    if (i == 2) {
-      image(cafe,tloc[i][0],tloc[i][1],cafe.width,cafe.height);
-    } else if (i == 3) {
-      image(temari,tloc[i][0] + 50,tloc[i][1] + 25,temari.width/2,temari.height/2);
-    } else {
-      rect(tloc[i][0],tloc[i][1],50,50);
-    }
+    imageMode(CENTER);
+    image(goods[i],tloc[i][0],tloc[i][1],goods[i].width,goods[i].height);
   }
 
-  image(sfran,(caw * (12/56)) + cax, height * (9/14),sfran.width * (4/7),sfran.height * (4/7));
-  image(marsh,(caw * (8/28)) + cax, height * (13/28),marsh.width,marsh.height);
-  image(kath,(caw * (3/5)) + cax, height * (3/7),kath.width,kath.height);
+  for (var i = 0; i < cloc.length; i++) {
+    // RNG
+    imageMode(CENTER);
+    image(cats[i],cloc[i][0],cloc[i][1],cats[i].width,cats[i].length);
+  }
 }
 
 // add eventlistener for window resize
